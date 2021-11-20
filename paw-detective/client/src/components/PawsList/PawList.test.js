7ccb5de1b8419fa4132ae8804cd0da1ca2470090
Aothe <[DLiamMorris96@gmail.com>
2021-11-20 11:40:17 +0000

    PawList test done to ensure proper render when list is empty & when list has an array of Paws

--- paw-detective/client/src/components/PawsList/PawList.test.js
+++ paw-detective/client/src/components/PawsList/PawList.test.js
@@ -1,10 +1,11 @@
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { StaticRouter } from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router';
 
-import PawList from './PawList.js';
+import PawsList from './PawList.js';
 import * as data from './mocks.json';
+import { BrowserRouter, StaticRouter } from 'react-router-dom';
 
 const singleData = [
   {
@@ -22,20 +23,71 @@ const singleData = [
     date: { $date: '2021-11-16T21:13:16.879Z' },
     __v: 0,
   },
+  {
+    _id: { $oid: '61941fb54bf9e3b1b5effde0' },
+    lostOrFound: 'Lost',
+    picture:
+      'https://firebasestorage.googleapis.com/v0/b/paw-detective-app.appspot.com/o/images%2Fraccoon.jpg?alt=media&token=07834b39-a0b9-4e85-ba04-e9540311923f',
+    animal: 'Other',
+    description:
+      'Our cheeky raccoon Ted has escaped again. He is small and friendly. If you have food with you he will definitely want to meet you',
+    location: 'Swords, Ireland',
+    lat: 53.45987775831185,
+    long: -6.212519573239081,
+    email: 'natasa410@hotmail.com',
+    date: { $date: '2021-11-16T21:16:37.424Z' },
+    __v: 0,
+  },
 ];
 
 describe('Paw list component test', () => {
-  test('Renders "there no pets..."', () => {
-    const container = document.createElement('div');
+  test('Renders "there no pets..."', async () => {
+    render(<PawsList paws={[]} />);
+    const items = await screen.findAllByText(/there no pets in this list😉/);
 
-    ReactDOM.render(<PawList items={[]} />, container);
-    expect(container.textContent).toMatch('there no pets in this list😉');
+    expect(items).toHaveLength(1);
   });
-  test('Renders a list of different pets from the data', () => {
+
+  test('Renders a list of different pets from the data', async () => {
+    // const items = render(
+    //   <StaticRouter>
+    //     <PawsList paws={singleData} />
+    //   </StaticRouter>
+    // );
+    // render(
+    //   // <StaticRouter>
+    //   <PawsList paws={singleData} />
+    //   // </StaticRouter>
+    // );
+    // const items = await screen.findAllByText(/Bird/);
+    // console.log(items);
+    // expect(items).toBe();
+    //----------------------------------------------------------
+    /*
     const container = document.createElement('div');
 
-    ReactDOM.render(<PawList items={data} />, container);
+    ReactDOM.render(
+      <StaticRouter>
+        <PawsList paws={singleData} key={25} />
+      </StaticRouter>,
+
+      container
+    );
+    console.log(container.textContent);
     expect(container.textContent).toMatch('Bird');
+    expect(container.textContent).toMatch('Other');
+    expect(container.textContent).toMatch('Dublin');
+    expect(container.textContent).toMatch('little blue parrot in my garden.');
+    */
+    //----------------------------------------------------------
+    render(
+      <StaticRouter>
+        <PawsList paws={singleData} />
+      </StaticRouter>
+    );
+    const items = await screen.findAllByText(/Bird/);
+
+    expect(items).toHaveLength(1);
   });
 });
 
